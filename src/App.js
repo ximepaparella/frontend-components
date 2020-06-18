7907a2040890f29fcc942ec97ea57b1385c269fa
Ximena Paparella <XPaparella@lanacion.com.ar>
2020-06-18 09:23:54 -0300

    Se suma pagina de repositorio

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Repositorio from './pages/repositorio';
 import NotaNoticia from './pages/nota-noticia';
 import NotaReceta from './pages/nota-receta';
 import NotaStorytelling from './pages/nota-storytelling';
+import Header from './modules/Header/index';
+import Footer from './modules/Footer/index';
 
 function App() {
   return (
-    <BrowserRouter>
-          <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/nota-noticia" component={NotaNoticia} />
-              <Route exact path="/nota-receta" component={NotaReceta} />
-              <Route exact path="/nota-storytelling" component={NotaStorytelling} />
-          </Switch>
-    </BrowserRouter>
+    <div className="wrapper">
+      <Header />
+      <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/repositorio" component={Repositorio} />
+                <Route exact path="/nota-noticia" component={NotaNoticia} />
+                <Route exact path="/nota-receta" component={NotaReceta} />
+                <Route exact path="/nota-storytelling" component={NotaStorytelling} />
+            </Switch>
+      </BrowserRouter>
+    <Footer />
+    </div>
   );
 }
 
